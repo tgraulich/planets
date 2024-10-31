07d5631f01784b9256706e86592f58ec39660bbf
Tim Aidan Graulich <63345846+tgraulich@users.noreply.github.com>
2024-10-31 16:19:48 +0100

    Create conf.py

---
+++ doc/conf.py
@@ -0,0 +1,16 @@
+project = "planets"
+copyright = "2024, Authors"
+author = "Authors"
+release = "0.1"
+
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+
+extensions = [
+    "myst_parser",  # in order to use markdown
+]
+
+myst_enable_extensions = [
+    "colon_fence",  # ::: can be used instead of ``` for better rendering
+]
+
+html_theme = "sphinx_rtd_theme"
